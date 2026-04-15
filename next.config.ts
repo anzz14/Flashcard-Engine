63e61a9194b458d0ca74904838c0f76938a365a7
anzz14 <anasnajam14@gmail.com>
2026-04-15 15:23:25 +0530

    prod bug

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["pdf-parse"],
+  serverExternalPackages: ["pdf-parse", "pdfjs-dist"],
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
   images: {
     remotePatterns: [
       {
